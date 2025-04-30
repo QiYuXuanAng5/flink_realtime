d44e1ea6e7526e2bfd49d727478f621cadc7600e
QiYuXuanAng5 <GZZH9731@outlook.com>
2025-04-30 12:39:50 +0800

    flink测试

--- flink.java
+++ flink.java
@@ -1,11 +1,11 @@
 /**
  * @Package PACKAGE_NAME.flink
- * @Author  guo.jia.hui
- * @Date  2025/4/30 11:15
- * @description: 
-*/
+ * @Author guo.jia.hui
+ * @Date 2025/4/30 11:15
+ * @description:
+ */
 public class flink {
     public static void main(String[] args) {
-
+        System.out.println(1);
     }
 }
